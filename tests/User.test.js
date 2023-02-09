020e7c84b745d15abff2265659a52014ea6a1063
DanielXFuller <danielxfuller@gmail.com>
2023-02-09 15:25:22 +0000

    checks the properties are all there in the tests

--- tests/User.test.js
+++ tests/User.test.js
@@ -14,5 +14,30 @@ const User = require('../src/User')
 
 
 describe('User', () => {
+    let user;
+
+    beforeEach(() => {
+        user = new User('john_doe', 'password123', 30)
+    });
+
+
+    describe('Has all properties', () => {
+
+        test('should have a username property', () => {
+            expect(user).toHaveProperty('username', 'john_doe');
+        });
+
+        test('should have a password property', () => {
+            expect(user).toHaveProperty('password', 'password123');
+        });
+
+        test('should have an age property', () => {
+            expect(user).toHaveProperty('age', 30);
+        });
+   
+    });
+
+
+
 
 });
