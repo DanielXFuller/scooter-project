2adc2bb5129203fd89183c5fbc2284bdd24f60d7
DanielXFuller <danielxfuller@gmail.com>
2023-02-10 11:00:49 +0000

    added serial number test to scooter

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -3,23 +3,63 @@ const User = require('../src/User')
 
 //typeof scooter === object
 describe('scooter object', () => {
-  test('does something', () => {
+  test('is an object', () => {
     // edit this to be a real test!
-    expect(false).toEqual(true);
+    const scooter = new Scooter('test Station');
+    expect(typeof scooter).toBe('object');
   }
 )
 })
 
 //Method tests
 describe('scooter methods', () => {
-  // tests here!
+  
+  let scooter;
+  let user;
+  let station;
 
-  //rent method
+  beforeEach(() => {
+    station = 'Test Station';
+    scooter = new Scooter(station);
+    user = new User('testuser', 'testpassword', 20);
+  });
 
-  //dock method
+  describe('Has all properties', () => { 
 
-  //requestRepair method
+    test('should have a station property', () => {
+      expect(scooter).toHaveProperty('station', 'Test Station');
+    });
 
-  //charge method
+    test('should have a user property', () => {
+      expect(scooter).toHaveProperty('user', null);
+    });
+
+    test('should have a serial property', () => {
+      expect(scooter).toHaveProperty('serial');
+    });
+
+    test('should have a charge property', () => {
+      expect(scooter).toHaveProperty('charge', 100);
+    });
+
+    test('should have a isBroken property', () => {
+      expect(scooter).toHaveProperty('isBroken', false);
+    });
+
+  });
+
+  describe('Scooter serial number', () => {
+
+    test('increments with each new Scooter', () => {
+
+      const scooter1 = new Scooter('station1');
+      const scooter2 = new Scooter('station2');
+      
+      expect(scooter2.serial).toBe(scooter1.serial + 1)
+    });
+
+  });
+
+
 
 })
